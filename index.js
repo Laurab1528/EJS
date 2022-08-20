fa78b537e9943bfa8655e08ccccf76221afe2e27
Laurab1528 <laurab1528@hotmail.com>
2022-08-19 22:28:07 -0500

    update

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const Contenedor = require('./Contenedor');
+const contenedor = new Contenedor('productos.json');
 
-const server = app.listen(PORT, () => {
-  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
-});
-
-server.on('error', (error) => console.log(`Error en servidor ${error}`));
-app.use('/public', express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set('view engine', 'ejs');
 
-let productsHC = [
-  { id: 1, title: 'nike ball', price: 101, thumbnail: 'http://localhost:8080/public/nike-ball.jpg' },
-  { id: 2, title: 'nike shoes', price: 102, thumbnail: 'http://localhost:8080/public/nike-shoes.jpg' },
-  { id: 3, title: 'adidas shoes', price: 102, thumbnail: 'http://localhost:8080/public/adidas-shoes.jpg' },
-];
-
-app.get('/products', (req, res) => {
-  res.render('pages/index', { title: 'listado de productos', products: productsHC });
+const server = app.listen(8080, () => {
+  console.log(`Servidor http iniciado en el puerto ${server.address().port}`);
+});
+server.on('error', (error) => {
+  console.log(`Error en el servidor ${error}`);
 });
 
-
-
-app.get('/form', (req, res) => {
-  res.render('pages/formulario', { title: 'Ingrese su producto' });
+app.get('/', (req, res) => {
+  res.render('pages/formulario.ejs');
 });
 
+app.post('/productos', async (req, res) => {
+  const { body } = req;
+  const { title, price, thumbnail } = body;
+  const producto = { title, price: parseInt(price), thumbnail };
+  await contenedor.save(producto);
+  res.redirect('/');
+});
+
+app.get('/productos', async (req, res) => {
+  const productos = await Contenedor.getAll();
+  if (productos.length !== 0) {
+    res.render('pages/index.ejs', { productos });
+  } 
+});
